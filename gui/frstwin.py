6e1c67b4795515f59537f7245f39e0ca2d5b138d
GeorgeKollin <47611955+GeorgeKollin@users.noreply.github.com>
2020-08-29 22:41:31 +0300

    Better compatibility with dark themes

--- gui/frstwin.py
+++ gui/frstwin.py
@@ -12,28 +12,28 @@ class InstallationWindow:
         self.window = Tk()
         # The given message should be at least 50 characters long!
         # Window title
-        self.window.title("Install Linux Nightly v3.0")
+        self.window.title("Install Linux Nightly v3.5")
         # Header image (if it doesn't show up, replace file="./icons/lxheader.png" with file="./icons/lxheader.gif" on line 17)
         headimg = PhotoImage(file="./icons/lxheader.png")
         hdlbl = Label(self.window, image=headimg)
         hdlbl.photo = headimg
         hdlbl.grid(row=0, column=0, columnspan=2)
         # Window messages
-        msgtoshow1 = Label(self.window, text="LINUX NIGHTLY V3.0.1341787\n\nLinux Nightly is a GUI program\nwritten for f.lux®. You can find\nmore information about f.lux® on\nits original website:")
+        msgtoshow1 = Label(self.window, text="LINUX NIGHTLY V3.5.1343D7D\n\nLinux Nightly is a GUI program\nwritten for f.lux®. You can find\nmore information about f.lux® on\nits original website:")
         msgtoshow1.grid(row=1, column=0, pady=(5, 5), columnspan=2)
         txt1 = StringVar(value="https://justgetflux.com/")
-        ent1 = Entry(self.window, text=txt1, state="readonly", width=24)
+        ent1 = Entry(self.window, text=txt1, state="readonly", width=24, fg="black")
         ent1.grid(row=2, column=0, columnspan=2)
-        msgtoshow2 = Label(self.window, text="© 2019 Linux Nightly.\nAll rights reserved.\nView source code on:")
+        msgtoshow2 = Label(self.window, text="© 2020 Linux Nightly.\nAll rights reserved.\nView source code on:")
         msgtoshow2.grid(row=3, column=0, pady=(10, 5), columnspan=2)
         txt2 = StringVar(value="https://github.com/georgekollin/lxnightly/")
-        ent2 = Entry(self.window, text=txt2, state="readonly", width=24)
+        ent2 = Entry(self.window, text=txt2, state="readonly", width=24, fg="black")
         ent2.grid(row=4, column=0, columnspan=2, pady=(0, 10))
         # "Exit" button
-        closebtn = Button(self.window, text="Exit", bg="white", command=self.noNormExit)
+        closebtn = Button(self.window, text="Exit", command=self.noNormExit)
         closebtn.grid(row=5, column=0, sticky="NSEW")
         # "Install" button
-        instbtn = Button(self.window, text="Install", bg="white", command=self.normExit)
+        instbtn = Button(self.window, text="Install", command=self.normExit)
         instbtn.grid(row=5, column=1, sticky="NSEW")
         self.window.update()
         # Center window
@@ -54,5 +54,7 @@ class InstallationWindow:
     def noNormExit(self):
         self.window.destroy()
         exit(1)
+
+InstallationWindow()
 
 InstallationWindow()
